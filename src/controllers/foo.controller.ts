de1152d3896a7cafcca740d776e72a54b63cbb90
ajardines <armando.jardines@netlogistik.com>
2019-01-29 19:14:24 -0600

    Hola

--- src/controllers/foo.controller.ts
+++ src/controllers/foo.controller.ts
@@ -4,11 +4,11 @@ import { interfaces, controller, httpGet} from "inversify-express-utils";
 import { injectable, inject } from "inversify";
 import { Request, Response } from "express";
  
-@controller("/foo")
+@controller("/")
 export class FooController implements interfaces.Controller {
 
   @httpGet("/")
   public index (request: Request, response: Response) {
-    response.status(200).send("Hola Mundo");
+    response.status(200).send("<h1>Hola equipo aprenDEH</h1>");
   }
 }
